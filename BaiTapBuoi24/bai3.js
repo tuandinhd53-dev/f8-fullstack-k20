a494f91aa338c2b98c8350761fd5babdf7375810
Dinh Tuan <tuandinhd53@gmail.com>
2026-07-25 23:55:19 +0700

    nopbaitapbuoi24 Errors, Classes

--- BaiTapBuoi24/bai3.js
+++ BaiTapBuoi24/bai3.js
@@ -1,87 +1,53 @@
-function createOrderSystem() {
-    const cart = [];
-
-    function addToCart(name, price, qty) {
-        cart.push({
-            name,
-            price,
-            qty,
-        });
-        return cart.length;
+class Employee {
+    constructor(name, baseSalary) {
+        this.name = name;
+        this.baseSalary = baseSalary;
     }
 
-    function checkout(distance) {
-        let shippingFee = 0;
-        const subtotal = cart.reduce((total, item) => {
-            return total + item.qty * item.price;
-        }, 0);
-        if (subtotal >= 500000) {
-            shippingFee = 0;
-        } else if (distance <= 5) {
-            shippingFee = 15000;
-        } else if (distance <= 20) {
-            shippingFee = 30000;
-        } else {
-            shippingFee = 50000;
-        }
-
-        const finalTotal = subtotal + shippingFee;
-
-        cart.length = 0;
-
-        return {
-            subtotal,
-            shippingFee,
-            finalTotal,
-        };
+    getMonthlySalary() {
+        return this.baseSalary;
     }
 
-    function getCartSize() {
-        return cart.length;
+    describe() {
+        return `${this.name} - Lương: ${this.baseSalary}đ`;
     }
-
-    return {
-        addToCart,
-        checkout,
-        getCartSize,
-    };
 }
 
-const store = createOrderSystem();
+class Manager extends Employee {
+    constructor(name, baseSalary, teamSize) {
+        super(name, baseSalary);
+        this.teamSize = teamSize;
+    }
 
-console.log(store.addToCart("Mũ lưỡi trai", 120000, 1));
-// 1  (số sản phẩm hiện có trong giỏ)
-console.log(store.getCartSize());
-// 1
+    getMonthlySalary() {
+        return super.getMonthlySalary() + this.teamSize * 500000;
+    }
 
-console.log(store.checkout(15));
+    describe() {
+        return `[Quản lý] ${super.describe()} (đội ${this.teamSize} người)`;
+    }
+}
 
-// { subtotal: 120000, shippingFee: 30000, finalTotal: 150000 }
+// Test
 
-console.log(store.getCartSize());
-// 0  (giỏ hàng đã tự làm trống sau khi thanh toán)
+const emp = new Employee("An", 10000000);
 
-// --- Một hệ thống khác, hoàn toàn độc lập ---
-const store2 = createOrderSystem();
+console.log(emp.getMonthlySalary()); // 10000000
+console.log(emp.describe()); // "An - Lương: 10000000đ"
 
-console.log(store2.addToCart("Tất", 30000, 2));
-// 1
-console.log(store2.checkout(3));
+const manager = new Manager("Bình", 15000000, 5);
+console.log(manager.getMonthlySalary());
 
-// { subtotal: 60000, shippingFee: 15000, finalTotal: 75000 }
+// 17500000   (15000000 + 5 * 500000)
 
-// --- Đơn hàng lớn, được miễn phí ship dù khoảng cách xa ---
-const store3 = createOrderSystem();
-console.log(store3.addToCart("Áo khoác", 600000, 1));
-// 1
-console.log(store3.checkout(30));
+console.log(manager.describe());
+// "[Quản lý] Bình - Lương: 17500000đ (đội 5 người)"
 
-// { subtotal: 600000, shippingFee: 0, finalTotal: 600000 }
+console.log(manager instanceof Employee);
+// true
 
-// store gốc và store2, store3 không hề ảnh hưởng lẫn nhau
-console.log(store.getCartSize());
-// 0
-console.log(store2.getCartSize());
-// 0
-console.log(store3.getCartSize());
-// 0
+console.log(manager instanceof Manager);
+// true
+
+console.log(emp instanceof Manager);
+// false
