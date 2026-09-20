b1d436ca13dc17182c66c8536376dfe64286499f
Dinh Tuan <tuandinhd53@gmail.com>
2026-09-20 22:16:51 +0700

    fix: add html entry points

--- Module-2/vite.config.js
+++ Module-2/vite.config.js
@@ -4,12 +4,15 @@ import { resolve } from "path";
 
 export default defineConfig({
     plugins: [tailwindcss()],
+
     build: {
         rollupOptions: {
             input: {
                 main: resolve(import.meta.dirname, "index.html"),
                 login: resolve(import.meta.dirname, "login.html"),
                 register: resolve(import.meta.dirname, "register.html"),
+                profile: resolve(import.meta.dirname, "profile.html"),
+                detail: resolve(import.meta.dirname, "detail.html"),
             },
         },
     },
